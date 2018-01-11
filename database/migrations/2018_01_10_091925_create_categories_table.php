275aa82e29f951ec65d0b0424193f8b15c89aa27
peace_data <kishetmmm@gmail.com>
2018-01-12 01:14:54 +0200

    fix tables

--- database/migrations/2018_01_10_091925_create_categories_table.php
+++ database/migrations/2018_01_10_091925_create_categories_table.php
@@ -17,7 +17,7 @@ class CreateCategoriesTable extends Migration
             $table->increments('id');
             $table->string('title');
             $table->string('slug')->unique();
-            $table->varchar('parent_id')->nullable();
+            $table->integer('parent_id')->nullable();
             $table->tinyInteger('published')->nullable();
             $table->integer('created_by')->nullable();
             $table->integer('modified_by')->nullable();
