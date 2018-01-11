4e0d7223d6a9aebc66afec9b3c84cdeef0a503b3
peace_data <kishetmmm@gmail.com>
2018-01-11 17:50:21 +0200

    add Edit Category

---
+++ resources/views/admin/categories/edit.blade.php
@@ -0,0 +1,24 @@
+@extends('admin.layouts.app_admin')
+
+@section('content')
+
+<div class="container">
+  @component('admin.components.breadcrumb')
+    @slot('title') Edit Category @endslot
+    @slot('parent') Main @endslot
+    @slot('active') Categories @endslot
+  @endcomponent
+  <hr/>
+
+  <form class="form-horizontal" action="{{route('admin.category.update', $category)}}" method="post">
+    <input type="hidden" name="_method" value="put">
+
+    {{csrf_field()}}
+
+    <!-- Form include -->
+    @include('admin.categories.partials.form')
+
+  </form>
+</div>
+
+@endsection
